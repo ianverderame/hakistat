6308c8d6732e954e649060282ce7c6b56edd3c19
Erik Bach <ebach615@gmail.com>
2026-04-06 12:36:47 -0700

    fixed but this time without sensitive info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://cidyutwgtxbqpkooikbd.supabase.co',
-  supabaseAnonKey: 'sb_publishable_gXbd0hHz2UOKlCmA0-2yhg_jDmzKpJ7'
+  supabaseUrl: '',
+  supabaseAnonKey: ''
 };
